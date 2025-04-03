24997fbb87fae443d3ba35885c0f3c7bde48282b
JerryS-15 <JerryS15@outlook.com>
2025-04-03 12:10:45 +0200

    Add data analyzer to get the amount of positive data in buffer.

---
+++ discrete_BCQ/data_analysis.py
@@ -0,0 +1,12 @@
+import numpy as np
+import os
+
+positive_threshold = 18
+reward_file = "buffers/Default_PongNoFrameskip-v4_0_reward.npy"
+rewards = np.load(reward_file)
+
+positive_mask = rewards > positive_threshold
+positive_count = np.sum(positive_mask)
+total_samples = len(rewards)
+
+print(f"Amount of Positive Data: {positive_count}/{total_samples} ({positive_count/total_samples:.2%})")
