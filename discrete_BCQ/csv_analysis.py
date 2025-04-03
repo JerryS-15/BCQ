1286b290773b63d309a04b4ca6bfa6471d4868e1
JerryS-15 <JerryS15@outlook.com>
2025-04-03 16:48:37 +0200

    Add episode reward analyser and .csv file from wandb.

---
+++ discrete_BCQ/csv_analysis.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import csv
+
+file_path = "./buffer_data/wandb_0401.csv"
+threshold = 18
+
+total_rows = 0
+over_threshold = 0
+
+with open(file_path, mode="r", encoding="utf-8") as f:
+    reader = csv.reader(f)
+    headers = next(reader)
+    # print("Headers:", headers)
+    for row in reader:
+        total_rows += 1
+        value = float(row[1])
+        if value > threshold:
+            over_threshold += 1
+
+print(f"Total number of episodes with rewards > {threshold} is: {over_threshold}/{total_rows} ({over_threshold/total_rows:.2%})")
